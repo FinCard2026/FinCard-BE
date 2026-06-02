bd2c9ee8e2ac9fd0d58205f7b521606b5140f8c2
seomunji <162777421+SEOMUNJI@users.noreply.github.com>
2026-06-02 13:56:38 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/example/fincard/FinCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.fincard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fincard/FinCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fincard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
